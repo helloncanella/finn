32c07abbe66f5f5cd7d2ad8ce8d34bafda8d929d
Hellon Canella <helloncanella@gmail.com>
2017-07-05 07:36:54 +0200

    tests are almost ok

--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -5,7 +5,8 @@ class Dashboard extends Component {
   constructor() {
     super()
     this.state = {
-      stepIndex: 0
+      stepIndex: 0,
+      error: ""
     }
 
     this.next = this.next.bind(this)
@@ -14,7 +15,6 @@ class Dashboard extends Component {
     this.onAdvance = this.onAdvance.bind(this)
 
     this.steps = []
-
   }
 
   next() {
@@ -27,13 +27,13 @@ class Dashboard extends Component {
   }
 
   onAdvance() {
-   
     const step = this.steps && this.steps[this.state.stepIndex]
 
     if (step) {
       try {
         step.validate && step.validate()
       } catch (e) {
+        this.setState({ error: e.message })
         console.error(e.message)
         return
       }
@@ -42,44 +42,108 @@ class Dashboard extends Component {
         this.props.save(step.getValues())
       }
 
-      if(!this.isLastStep()) this.next()
+      if (!this.isLastStep()) this.next()
     }
   }
 
-  isLastStep = () =>{   
-    return this.state.stepIndex === (this.steps.length-1)
+  isLastStep = () => {
+    return this.state.stepIndex === this.steps.length - 1
   }
-
-
 
   getChildren() {
-    let { children } = this.props
-   
-    const allChildren = Array.isArray(children) ? children : [children]
+    return React.Children.map(this.props.children, (child, index) => {
+      const isNotCurrentStep = index !== this.state.stepIndex
+      
+      const style = { display: isNotCurrentStep ? "none" : "block" }
+      
+      const props = {
+        ...child.props,
+        style,
+        ref: e => !this.steps[index] && this.steps.push(e)
+      }
 
-    return allChildren.map((child,index) => {
-      let props = child.props || {}   
-   
-      return React.cloneElement(child, {
-        ...props,
-        style: {display: index !== this.state.stepIndex && 'none'},
-        ref: e => {
-           this.steps.push(e)
-        }
-      })
+      return React.cloneElement(child, props)
     })
-
   }
 
-  componentDidUpdate(){
-    console.log(this.children[1].props)
+  controls() {
+    const back =
+      this.state.stepIndex > 0 &&
+      <div className="back button" onClick={this.back}>
+        Zurück
+      </div>
+
+    const next = (
+      <div className="next button" onClick={this.onAdvance}>
+        {" "}Speichern
+      </div>
+    )
+
+    return (
+      <div className="controls row">
+        <h6 className="error">
+          {this.state.error}
+        </h6>
+        {back}
+        {next}
+      </div>
+    )
+  }
+
+  stepsComponents() {
+    const children = this.getChildren()
+    return (
+      <div className="children row">
+        {children}
+      </div>
+    )
+  }
+
+  top() {
+    return (
+      <div className="top row">
+        <div className="profile small-3 column">
+          <h3 className="title">Profil Ansehen</h3>
+          <h4>überblick über eigene Daten</h4>
+          <img
+            src="https://www.w3schools.com/w3css/img_avatar3.png"
+            alt="avatar"
+            className="avatar"
+          />
+        </div>
+        <div className="all-steps small-9 column">
+          <h3 className="title">Editieren</h3>
+          <div className="sequences">
+            <div className="line" />
+            <div className="steps">
+              <div className="step current">
+                <div className="bullet" />
+                <h6 className="description">Profil/Kontak</h6>
+              </div>
+              <div className="step">
+                <div className="bullet" />
+                <h6 className="description">Fotos</h6>
+              </div>
+              <div className="step">
+                <div className="bullet" />
+                <h6 className="description">Socialmedia</h6>
+              </div>
+              <div className="step">
+                <div className="bullet" />
+                <h6 className="description">Service</h6>
+              </div>
+              <div className="step">
+                <div className="bullet" />
+                <h6 className="description">Zip/Anhang</h6>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
   }
 
   render() {
-    if(!this.children) this.children = this.getChildren()
-
-    
-
     return (
       <div className="layout">
         <aside />
@@ -87,63 +151,15 @@ class Dashboard extends Component {
           <header />
           <div className="dashboard">
             <div className="stepper">
-              <div className="top row">
-                <div className="profile small-3 column">
-                  <h3 className="title">Profil Ansehen</h3>
-                  <h4>überblick über eigene Daten</h4>
-                  <img
-                    src="https://www.w3schools.com/w3css/img_avatar3.png"
-                    alt="avatar"
-                    className="avatar"
-                  />
-                </div>
-                <div className="all-steps small-9 column">
-                  <h3 className="title">Editieren</h3>
-                  <div className="sequences">
-                    <div className="line" />
-                    <div className="steps">
-                      <div className="step current">
-                        <div className="bullet" />
-                        <h6 className="description">Profil/Kontak</h6>
-                      </div>
-                      <div className="step">
-                        <div className="bullet" />
-                        <h6 className="description">Fotos</h6>
-                      </div>
-                      <div className="step">
-                        <div className="bullet" />
-                        <h6 className="description">Socialmedia</h6>
-                      </div>
-                      <div className="step">
-                        <div className="bullet" />
-                        <h6 className="description">Service</h6>
-                      </div>
-                      <div className="step">
-                        <div className="bullet" />
-                        <h6 className="description">Zip/Anhang</h6>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="children row">
-                {this.children}
-              </div>
-              <div className="controls row">
-                {this.state.stepIndex > 0 &&
-                  <div className="back button" onClick={this.back}>
-                    Zurück
-                  </div>}
-                <div className="next button" onClick={this.onAdvance}>
-                  {" "}Speichern
-                </div>
-              </div>
+              {this.top()}
+              {this.stepsComponents()}
+              {this.controls()}
             </div>
           </div>
         </main>
       </div>
     )
-  }
+  } 
 }
 
 export default Dashboard
--- src/Dashboard/__tests__/Stepper-test.js
+++ src/Dashboard/__tests__/Stepper-test.js
@@ -1,24 +1,24 @@
-import React, {Component} from "react"
+import React, { Component } from "react"
 import { shallow, mount } from "enzyme"
 import Dashboard from "../Dashboard"
 
 const wrapper = (props = {}, toMount) => {
   props.children = props.children || <div />
   const component = <Dashboard {...props} />
-  return toMount=='mount' ? mount(component) : shallow(component)
+  return toMount == "mount" ? mount(component) : shallow(component)
 }
 
-class Stub extends Component{
-  validate(){
+class Stub extends Component {
+  validate() {
     this.props.validate && this.props.validate()
   }
 
-  getValues(){
+  getValues() {
     return 2
   }
 
-  render(){
-    return <div></div>
+  render() {
+    return <div />
   }
 }
 
@@ -26,15 +26,15 @@ describe("rendering steps", () => {
   let comp,
     clickBack = () => comp.find(".back").simulate("click"),
     clickNext = () => comp.find(".next").simulate("click"),
-    setState = (state) =>comp.setState(state),
-    state = ()=>comp.state()
+    setState = state => comp.setState(state),
+    state = () => comp.state()
 
   beforeEach(() => {
     comp = wrapper()
   })
 
   afterEach(() => (comp = null))
- 
+
   it("Just one children is rendered", () => {
     const children = [<div id="1" key="1" />, <div id="2" key="2" />]
     const allChildren = wrapper({ children }).find(".children").props().children
@@ -42,12 +42,12 @@ describe("rendering steps", () => {
     const child1StyleDisplay = allChildren[0].props.style.display
     const child2StyleDisplay = allChildren[1].props.style.display
 
-    expect(child1StyleDisplay).not.toBe('none')
-    expect(child2StyleDisplay).toBe('none')
+    expect(child1StyleDisplay).not.toBe("none")
+    expect(child2StyleDisplay).toBe("none")
   })
 
   it("next button, advance step", () => {
-    comp=wrapper({children:[<Stub/>, <Stub/>]}, 'mount')
+    comp = wrapper({ children: [<Stub key="1"/>, <Stub key="2"/>] }, "mount")
     clickNext()
     expect(state().stepIndex).toEqual(1)
   })
@@ -58,68 +58,80 @@ describe("rendering steps", () => {
     expect(state().stepIndex).toEqual(4)
   })
 
- 
   it("the back button doesn't appear in the first step", () => {
-    setState({stepIndex:0})
-    expect(comp.find('.back').length).toEqual(0)
+    setState({ stepIndex: 0 })
+    expect(comp.find(".back").length).toEqual(0)
   })
 
   it("once confirm button is pressed it validates the fields", () => {
-    const validation = jest.fn(()=>{})
+    const validation = jest.fn(() => {})
     const children = <Stub validate={validation} />
-    
-    comp = wrapper({children}, 'mount')
+
+    comp = wrapper({ children }, "mount")
     clickNext()
 
     expect(validation).toBeCalled()
   })
 
-  it("if field is valid, collect data and pass to save function", ()=>{
+  it("if field is valid, collect data and pass to save function", () => {
     const values = {}
-    Stub.prototype.getValues = jest.fn(()=>values)
-  
-    const save = jest.fn()
-    const children = <Stub validate={()=>{}}/> 
+    Stub.prototype.getValues = jest.fn(() => values)
 
-    comp = wrapper({children, save}, 'mount')
-    
+    const save = jest.fn()
+    const children = <Stub validate={() => {}} />
+
+    comp = wrapper({ children, save }, "mount")
+
     clickNext()
 
     expect(save).toBeCalledWith(values)
   })
 
+  it("dont increases stepIndex if it is the last step", () => {
+    const children = [<Stub key="1"/>, <Stub key="2"/>, <Stub key="3"/>]
 
-  it('don\'t advance if step validation fails', ()=>{
-    const error = 'blac'
-    
-    const validate = jest.fn(()=>{
-      throw new Error(error)
-    })
+    comp = wrapper({ children }, "mount")
 
-    comp = wrapper({children: <Stub validate={validate} />}, 'mount')
-    
     clickNext()
-
-    expect(state().stepIndex).toBe(0)
-  })
-
-  it.only('dont increases stepIndex if it is the last step', ()=>{
-    
-    const children = [<Stub/>, <Stub />, <Stub />]
-
-    comp = wrapper({children}, 'mount')
-  
-  
+    clickNext()
     clickNext()
     clickNext()
     clickNext()
     clickNext()
 
     expect(comp.instance().state.stepIndex).toBe(2)
-
-
   })
 
+  it("visibility of current step changes, when button is pressed", () => {
+    const children = [<Stub key="1"/>, <Stub key="2"/>]
 
- 
+    comp = wrapper({ children }, "mount")
+
+    const display = () =>
+      comp.find(".children").props().children[0].props.style.display
+
+    clickNext()
+    expect(display()).toBe("none")
+  })
+
+  describe("if validation fails", () => {
+    let error = "black"
+    let validate
+
+    beforeEach(() => {
+      validate = jest.fn(() => {
+        throw new Error(error)
+      })
+      comp = wrapper({ children: <Stub validate={validate} /> }, "mount")
+      clickNext()
+    })
+
+    it("print error", () => {
+      expect(comp.find(".error").text()).toBe(error)
+    })
+
+    it("don't advance", () => {
+      expect(state().stepIndex).toBe(0)
+    })
+  })
 })
