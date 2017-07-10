0b5943f52f93599224f74fae5a24da1228493277
Hellon Canella <helloncanella@gmail.com>
2017-07-10 06:23:40 +0200

    input is ok

--- src/app.jsx
+++ src/app.jsx
@@ -2,20 +2,36 @@ import React, { Component } from "react"
 import HomePage from "./HomePage/Index.jsx"
 import Edit from "./Edit/Edit.jsx"
 
-import "./Edit/_edit.scss"
-import "./OldEdit/editor.scss"
+// import "./Edit/_edit.scss"
+// import "./OldEdit/editor.scss"
 
 import anbieter from "./Edit/fake-anbieter.json"
 import servicesList from "./Edit/services-list.json"
 
+const edit = (
+  <Edit
+    user={anbieter}
+    saveUser={oi => console.log(oi)}
+    anbieterServices={servicesList}
+  />
+)
+
+import "./ImageInput/_image-input.scss"
+import ImageInput from "./ImageInput/ImageInput"
+
 export default class App extends React.Component {
   render() {
-    return (
-      <Edit
-        user={anbieter}
-        saveUser={oi => console.log(oi)}
-        anbieterServices={servicesList}
+    const imageInput = (
+      <ImageInput
+        id="123132132"
+        preview={getImage("kklkdla")}
+        saveImage={image => console.log(image)}
+        deleteImage={(i,callback) => {console.log(i);callback()}}
+        ref={e=>this.input =e }
       />
     )
+    return imageInput
   }
 }
+
+const getImage = ()=>"https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Rio_De_Janeiro_-_Rafael_Defavari.jpg/700px-Rio_De_Janeiro_-_Rafael_Defavari.jpg"
